ed2641b52ef5f1f13e030aa26ac12172a059dd0f
Aditya Naresh <adityanaresh007@gmail.com>
2025-01-05 05:24:10 +0530

    django set up added above

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,10 +1,12 @@
 from django.core.management import call_command
-
+import django
 import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
+django.setup()
 
 try:
     call_command("migrate")
