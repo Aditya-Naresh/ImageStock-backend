4fee52240b91b3b0173b24b34ad35b0b432bdfc6
Aditya Naresh <adityanaresh007@gmail.com>
2025-01-05 14:09:23 +0530

    celery url changed

--- mysite/celery.py
+++ mysite/celery.py
@@ -10,11 +10,13 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 app = Celery("mysite")
 
 app.conf.update(
-    broker_user_ssl=True,
+    broker_url="redis://red-ctstd68gph6c738ee8d0:6379",
     broker_transport_options={
-        'ssl_cert_reqs': ssl.CERT_NONE
-    }
+        "ssl_cert_reqs": ssl.CERT_NONE,
+        "socket_timeout": 30
+    },
 )
+
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
 app.autodiscover_tasks()
